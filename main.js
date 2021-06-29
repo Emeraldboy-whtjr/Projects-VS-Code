db207fcd40611f37adf0b34ce09668331a29bc6f
Emeraldboy-whtjr <82824596+Emeraldboy-whtjr@users.noreply.github.com>
2021-06-29 23:35:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var first_name = "Ranbir";
+var last_name = "kapoor";
+var fullname = firstname.concat(last_name);
+
+console.log(first_name);
+console.log(last_name);
+console.log(fullname);
+
+var first_number = 14;
+var second_number = 6;
+
+console.log(first_number + second_number);
